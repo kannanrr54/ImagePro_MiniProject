92fcd1ad86da5bdbc6a25e84923b6ab2a1b26776
Kannan R R <kannanrr54@gmail.com>
2022-10-20 20:44:40 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,8 @@ function app(props){
     <div>
         <form>
         <div>
-          <input type="file" onChange={getImage} required=""/><br/><br/>
-          <select id="type" required="">
+          <input type="file" onChange={getImage} /><br/><br/>
+          <select id="type">
             <option value="WEBP">WEBP</option>
             <option value="JPEG2">JPEG2</option>
             <option value="JPEGXR">JPEGXR</option>
