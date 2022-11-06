3a6c1f3582bc2ed063292c244cc9b74856125367
Kannan R R <kannanrr54@gmail.com>
2022-11-06 12:16:03 +0530

    lot of updates

--- api/app.py
+++ api/app.py
@@ -1,8 +1,11 @@
-import os,glob
+import os,cloudinary,cloudinary.uploader,urllib,urllib.request
 from tokenize import String
 from flask import Flask, request
 from PIL import Image
 from PIL.ExifTags import TAGS
+from PIL import Image
+import requests
+from io import BytesIO
 
 UPLOAD_FOLDER = '../'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -12,24 +15,49 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 path='public'
 type=""
 exif=""
+c=""
+destination=""
+
 @app.route('/upload', methods=['POST'])
 def fileUpload():
     target=os.path.join(UPLOAD_FOLDER,path)
     if not os.path.isdir(target):
-        os.mkdir(target) 
-    file = request.files['file']
-    type = request.form['type']
-    img = Image.open(file)
-    exif = { TAGS[k]: v for k, v in img._getexif().items() if k in TAGS }
-    print(exif)
-    if(type=='JPEG2'):
-        type='j2k'
+        os.mkdir(target)
     fname=request.form['filename']
-    img=Image.open(file)
-    destination="/".join([target, fname+"."+type])
-    # if os.path.isfile(destination):
-    #     os.remove(destination)
-    img.save(destination)
+    type = request.form['type']
+    if(request.form['url']!=""):
+        url = request.form['url']
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+
+    else:
+        file = request.files['file']
+        img = Image.open(file)
+    if(type!='wdp'):
+        destination="/".join([target, fname+"."+type])
+        img.save(destination)
+    else:
+        destination="/".join([target, fname+".webp"])
+        img.save(destination)
+
+    #---------- JPEG-XR Conversion using Cloudinary ----------
+    if(type=='wdp'):
+        cloudinary.config(
+        cloud_name = "dwdym3doh",
+        api_key = "724199248487985",
+        api_secret = "1jEFu1JeEXI0uR6X1drb7zyyENA"
+        )
+        cloudinary.uploader.upload(destination,public_id=fname, unique_filename = True, overwrite=True)
+        srcURL = cloudinary.CloudinaryImage(fname).image( format = "jxr")
+        r = urllib.request.urlopen(srcURL.split('"')[1])
+        save_path = '../public/'
+        completeName = os.path.join(save_path, fname+".wdp")   
+        file1 = open(completeName, "wb")
+        print("-------------------------"+completeName)
+        file1.write(r.read())
+        file1.close()
+        # delete Image cloudinary
+        # cloudinary.uploader.destroy('zombie', function(result) { console.log(result) });
     return  {}
 
 @app.route('/getimage')
@@ -38,11 +66,18 @@ def getimage():
 
 @app.route('/getMetadata',methods=['POST','GET'])
 def getMeta():
-    meta="{"
+    file = request.files['file']
+    target=os.path.join(UPLOAD_FOLDER,path)
+    fname=request.form['filename']
+    type = request.form['type']
+    img = Image.open(file)
+    # destination="/".join([target, fname+".jpg"])
+    # rgb_im = img.convert('RGB')
+    # rgb_im.save(destination)
+    meta="{"    
     a=0
     z=0
-    file = request.files['file']
-    img = Image.open(file)
+    # img = Image.open(destination)
     exif = { TAGS[k]: v for k, v in img._getexif().items() if k in TAGS }
     for i in exif:
         a=a+1
@@ -53,6 +88,6 @@ def getMeta():
         else:
             meta=meta+'"'+str(i)+'":"'+str(exif.get(i))+'"'
     meta=meta+"}"
-    print(meta)
     meta=meta.replace("\\","")
+
     return {"meta":meta}
--- pages/index.js
+++ pages/index.js
@@ -1,140 +1,182 @@
-import React, {
-  useState,
-  useEffect
-} from 'react';
-import Image from 'next/image';
+import React, {	useState,useEffect} from 'react';
+
+/*to-do
+check url is valid*/
 
 function app(props) {
-  const data = null
-  const f = 0
-  const type = null
-  const filename="",file=""
-  const download=0
+	const filename = "",file = "",url = "",download = 0,link = 0,type = null,f = 0,data = null
 
-  const getImage = (ev) => {
-      f = 1;
-      file=ev.target.files[0];
-      filename=document.getElementById('images').value.split(/[.]/)[0].split('\\');
-      filename=filename[filename.length-1]
-      data = new FormData();
-      data.delete('file');
-      data.append('file', file);
-      data.append('filename',filename);
-  }
-  const handleUploadImage = (ev) => {
-      ev.preventDefault();
-      document.getElementById("img").setAttribute("src", 'loading.gif');
-      if (f != 0) {
-          var type=document.getElementById('type').value;
-          if(type=='JPEG2'){
-            type='j2k';
-          }
-          data.append('type', document.getElementById("type").value)
-          fetch('/upload', {
-              method: 'POST',
-              mode: 'no-cors',
-              body: data,
-          }).then((response) => {
-              response.json().then((body) => {
-              });
-              if(response.status == 200){
-                fetch('/getMetadata', {
-                  method: 'POST',
-                  mode: 'no-cors',
-                  body: data,
-              }).then((response) => {
-                  response.json().then((body) => {
-                    var mData=JSON.parse(body.meta);
-                    console.log(mData.Model)
-                    const title = ["Make", "Model", "DateTimeOriginal","FocalLength","ISOSpeedRatings"];
-                    var table= document.getElementById("metaData");
-                    var metaDataRows=""
-                    title.forEach(element => {
-                      metaDataRows=metaDataRows+"<tr><td>"+element+"</td><td>"+mData[element]+"</td></tr>"
-                    });
-                    
-                    table.innerHTML=metaDataRows;
-                    
-                  });
-              });
-                document.getElementById("img").setAttribute("src", filename+"."+type);
-                document.getElementById("img_title").innerHTML="CONVERTED IMAGE : "+filename+"."+type; 
-                // document.getElementById('dld').removeAttribute('disabled');
-                download=1
-              }
-          });
-          f = 0             
-      }
-  }
-  const getImg = (ev) => {
-      ev.preventDefault();
-      if(download==1){
-        fetch('/getimage').then((response) => {
-          response.json().then((body) => {
-              var type=document.getElementById('type').value;
-              if(type=='JPEG2'){
-                type='j2k';
-              }
-              var link = document.createElement('a');
-              link.href = document.getElementById("img").getAttribute("src");
-              link.download = filename +'.' + type;
-              document.body.appendChild(link);
-              link.click();
-              document.body.removeChild(link);
-              // document.getElementById('dld').setAttribute('disabled');
-          });
-      });
-      }
-      download=0;
-  }
-  const meta = (ev) => {
-      //call get api to get meta data
-  }
+	const getImageLink = (ev) => {
+		link = 1
+		document.getElementById("images").value = "";
+		data = new FormData();
+		f=1;
+	}
+
+	const getImage = (ev) => {
+		f = 1;
+		document.getElementById("link").value = "";
+		data = new FormData();
+		file = ev.target.files[0];
+		data.delete('file');
+		data.append('file', file);
+	}
+
+	const handleUploadImage = (ev) => {
+		ev.preventDefault();
+		document.getElementById("img").setAttribute("src", 'loading.gif');
+		if (link) {
+			url = document.getElementById('link').value;
+			filename = url.split("/")[url.split("/").length - 1].split('.')[0	];
+			data.delete('url',url);
+			data.append('url',url);
+			console.log(filename)
+		} else {
+			filename = document.getElementById('images').value.split(/[.]/)[0].split('\\');
+			filename = filename[filename.length - 1]
+			data.delete('url',url);
+			data.append('url',"");
+		}
+		data.delete('filename', filename);
+		data.append('filename', filename);
+		
+		if (f != 0) {
+			var type = document.getElementById('type').value;
+			if (type == 'JPEG2') {
+				type = 'j2k';
+			} else if (type == 'JPEGXR') {
+				type = 'wdp';
+			}
+			console.log(data)
+			data.append('type', type)
+			fetch('/upload', {
+				method: 'POST',
+				mode: 'no-cors',
+				body: data,
+			}).then((response) => {
+				
+				response.json().then((body) => {});
+				if (response.status == 200) {
+					fetch('/getMetadata', {
+						method: 'POST',
+						mode: 'no-cors',
+						body: data,
+					}).then((response) => {
+						response.json().then((body) => {
+							var mData = JSON.parse(body.meta);
+							const title = ["Make", "Model", "DateTimeOriginal", "FocalLength", "ISOSpeedRatings"];
+							var table = document.getElementById("metaData");
+							var metaDataRows = ""
+							title.forEach(element => {
+								metaDataRows = metaDataRows + "<tr><td>" + element + "</td><td>" + mData[element] + "</td></tr>"
+							});
+							table.innerHTML = metaDataRows;
+							console.log(metaDataRows);
+						});
+					});
+					// if(type=="j2k" || type=="wdp"){
+					// 	document.getElementById("img").setAttribute("src", "img-def.png");	
+					// }
+					// else{
+						document.getElementById("img").setAttribute("src", filename + "." + type);
+					// }
+					document.getElementById("img_title").innerHTML = "CONVERTED IMAGE : " + filename + "." + type;
+					download = 1
+				}
+			});
+			f = 0
+		}
+	}
+	const getImg = (ev) => {
+		ev.preventDefault();
+		if (download == 1) {
+			fetch('/getimage').then((response) => {
+				response.json().then((body) => {
+					var type = document.getElementById('type').value;
+					if (type == 'JPEG2') {
+						type = 'j2k';
+					} else if (type == 'JPEGXR') {
+						type = 'wdp';
+					}
+					var link = document.createElement('a');
+					link.href = document.getElementById("img").getAttribute("src");
+					link.download = filename + '.' + type;
+					document.body.appendChild(link);
+					link.click();
+					document.body.removeChild(link);
+				});
+			});
+		}
+		download = 0;
+	}
   return(
     <>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <title>ImagePro</title>
-    <div className='head1'>
-        <p className='title'>ImagePro</p>
-    </div>
-    <div className='head2'>
-        <p className='catch'>Convert on the Go!</p>
-    </div>
-    <div className='outer1'>
-        <label htmlFor="images" className="drop-container">
-            <span className="drop-title">Drop files here</span>
-            or
-            <input type="file" id="images" accept="image/*" onChange={getImage} />
-        </label>
-        <select id="type" className='dd'>
-            <option value="">SELECT A TYPE TO CONVERT</option>
-            <option value="WEBP">WEBP</option>
-            <option value="JPEG2">JPEG2</option>
-            <option value="JPEGXR">JPEGXR</option>
-        </select><br /><br />
-        <input type="submit" onClick={handleUploadImage} className="btn btn-primary" id='form_ele' value='CONVERT'/><br />
-        <center>
-            <button onClick={getImg} className="btn btn-primary" id='dld'>
-                DOWNLOAD
-            </button><br/>
-        </center>
-
-    </div>
-    <div className='outer2'>
-        <div id='img_title'>CONVERTED IMAGE :</div>
-        <img id="img" className='img' /><br/><br/>
-        <div className='meta'>Metadata
-        <table>
-          <tbody id="metaData">
-          </tbody>
-          
-      </table>
-        </div>
-    </div>
-
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+ <title>ImagePro</title>
+ <div className='head1'>
+   <p className='title'>ImagePro</p>
+ </div>
+ <div className='head2'>
+   <p className='catch'>Convert on the Go!</p>
+ </div>
+ <div className='outer1'>
+   <label htmlFor="images" className="drop-container">
+     <span className="drop-title">
+ 		<input type="text" className="link" id="link" onChange={getImageLink} placeholder="Enter the URL" />       
+     	</span> or <input type="file" id="images" accept="image/*" onChange={getImage} />
+   </label>
+   <select id="type" className='dd'>
+     <option value="">SELECT A TYPE TO CONVERT</option>
+     <option value="WEBP">WEBP</option>
+     <option value="JPEG2">JPEG2</option>
+     <option value="JPEGXR">JPEGXR</option>
+   </select>
+   <br />
+   <br />
+   <input type="submit" onClick={handleUploadImage} className="btn btn-primary" id='form_ele' value='CONVERT' />
+   <br />
+   <center>
+     <button onClick={getImg} className="btn btn-primary" id='dld'> DOWNLOAD </button>
+     <br />
+   </center>
+ </div>
+ <div className='outer2'>
+   <div id='img_title'>CONVERTED IMAGE :</div>
+   <picture>
+   		<img id="img" className='img' src="img-def.png"/>
+   </picture>
+   
+   <br />
+   <br />
+   <div className='meta' id="a">Metadata <table>
+       <tbody id="metaData">
+	   	<tr>
+			<td>Make</td>
+			<td>   -   </td>
+		</tr>
+		<tr>
+			<td>Model</td>
+			<td>   -   </td>
+		</tr>
+		<tr>
+			<td>DateTimeOriginal</td>
+			<td>   -   </td>
+		</tr>
+		<tr>
+			<td>FocalLength</td>
+			<td>   -   </td>
+		</tr>
+		<tr>
+			<td>ISOSpeedRatings</td>
+			<td>   -   </td>
+		</tr>
+	   </tbody>
+     </table>
+   </div>
+ </div>
 </>
   )
 }
