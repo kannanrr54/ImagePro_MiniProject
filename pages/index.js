255946551755b361a07512ef313cf66b54849711
Kannan R R <kannanrr54@gmail.com>
2022-11-07 22:14:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -113,11 +113,11 @@ let myscript=document.createElement('script');
 	}
   return(
     <>
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
- <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" async/>
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js" async></script>
  <Script src="https://third-party-script.js" async />
- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" async></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" async></script>
  <title>ImagePro</title>
  <div className='head1'>
    <p className='title'>ImagePro</p>
