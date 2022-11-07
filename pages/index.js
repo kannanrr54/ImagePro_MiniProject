81de0f1832198e20c31ea57747719bf6cc935a77
Kannan R R <kannanrr54@gmail.com>
2022-11-07 15:53:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ function app(props) {
     <>
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
- <script src="https://third-party-script.js" defer />
+ <script src="https://third-party-script.js" async />
  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
  <title>ImagePro</title>
