8e38e32c8e1b9b7f963fe3b4e5207cbdf016abae
Kannan R R <kannanrr54@gmail.com>
2022-11-07 15:50:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, {	useState,useEffect} from 'react';
+import Script from 'next/script';
 
 /*to-do
 check url is valid*/
@@ -113,6 +114,7 @@ function app(props) {
     <>
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+ <Script src="https://third-party-script.js"></Script>
  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
  <title>ImagePro</title>
